63b8a1ff01012a1774abfcc89ee6b06f4a618366
mnayeembasha <mnayeembasha@gmail.com>
2025-03-08 11:45:18 +0530

    fixed logout issue

--- src/routes/questionsroute.ts
+++ src/routes/questionsroute.ts
@@ -3,7 +3,6 @@ import {  runCode } from '../controller/runCode';
 import { submitCode } from '../controller/submitCode';
 import { question, getspecificquestion } from '../controller/getspecificquestion';
 import { addQuestion } from "../controller/addQuestion";
-import { isAuthenticated } from "../middleware/authMiddleware";
 import { isTeckziteUser } from "../middleware/teckziteUserMiddleware";
 import { RequestHandler } from "express";
 const router = express.Router();
@@ -11,7 +10,7 @@ const router = express.Router();
 router.get("/questions", question);
 router.get("/questions/:id", getspecificquestion);
 router.post("/questions/add",addQuestion);
-router.post("/run",runCode);
+router.post("/run",isTeckziteUser as RequestHandler,runCode);
 router.post("/submit", submitCode);
 
 export default router;
