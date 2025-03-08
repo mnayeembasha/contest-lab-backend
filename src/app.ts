c8fa85c02f521e148d1fcfc3d3b6ecd81dd097eb
mnayeembasha <mnayeembasha@gmail.com>
2025-03-08 13:30:01 +0530

    fixed cors issue

--- src/app.ts
+++ src/app.ts
@@ -22,18 +22,24 @@ app.use(cookieParser());
 
 const allowedOrigins = ["http://localhost:3001", "https://contest-lab.vercel.app"];
 
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin || allowedOrigins.includes(origin)) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//   })
+// );
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: true, // Allows all origins dynamically
+    credentials: true, // Important for cookies
   })
 );
 
